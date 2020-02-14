a3da3a2b2ea19cdbad7f5c7c73cf6b227c14bec6
2330647 <alfonsofezza93@gmail.com>
2020-02-14 17:18:47 +0100

    addedd DEVICE_NUM and MSG_SIZE define

--- inc/common/config.h
+++ inc/common/config.h
@@ -1,6 +1,9 @@
 #ifndef CH_CONF
 #define CH_CONF
 
-#define CH_TH_WAIT_PERIOD 50
+#define CH_TH_WAIT_PERIOD   50
+#define DEVICE_NUM          3
+#define MSG_SIZE            4
+
 
 #endif /* CH_CONF*/
