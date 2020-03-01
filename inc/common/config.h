6879a7b8e064d8d4a616a602ac89671d8407fc0b
2330647 <alfonsofezza93@gmail.com>
2020-03-01 18:27:00 +0100

    added new define

--- inc/common/config.h
+++ inc/common/config.h
@@ -1,9 +1,13 @@
 #ifndef CH_CONF
 #define CH_CONF
 
-#define CH_TH_WAIT_PERIOD   50
-#define DEVICE_NUM          3
-#define MSG_SIZE            4
+#include "phemap_message.h"
 
+#define CH_TH_WAIT_PERIOD   50
+#define DEVICE_NUM          2
+#define MSG_SIZE            sizeof(PHEMAP_Message_t)
+#define SENTINEL            4
+#define DEV_FANIN           1
+#define DEV_FANOUT          0
 
 #endif /* CH_CONF*/
