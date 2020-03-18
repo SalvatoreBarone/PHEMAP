948bc45a6c45f26d07e4553d8c587d9e8762905f
2330647 <alfonsofezza93@gmail.com>
2020-03-18 17:18:39 +0100

    changed references for using entity information

--- src/device/phemap_dev_phi.c
+++ src/device/phemap_dev_phi.c
@@ -56,11 +56,12 @@
  */
 uint32_t PHEMAP_Device_peekLink(
 		PHEMAP_Device_t * const device,			//necessario per la PUF
+		PHEMAP_Entity_t * const entity,
 		uint32_t i,
 		uint8_t senti_flag,
 		PHEMAP_Link_t * const link)				//challenge e response
 {
-	uint32_t counter = device->counter;
+	uint32_t counter = entity->counter;
 	for (uint32_t k = 0; k < i; k++)
 	{
 		if (senti_flag != RET_SENTINEL) //non voglio le sentinelle 
@@ -87,20 +88,21 @@ uint32_t PHEMAP_Device_peekLink(
 
 uint32_t PHEMAP_Device_getNextLink(
 		PHEMAP_Device_t * const device,			//necessario per la PUF
+		PHEMAP_Entity_t * const entity,
 		uint8_t senti_flag,
 		PHEMAP_Link_t * const link)				//contiene il successivo link nella chain
 {
 	if (senti_flag != RET_SENTINEL) //non voglio le sentinelle 
 		{
-			if ((device->counter % SENTINEL) == 0)
+			if ((entity->counter % SENTINEL) == 0)
 			{
 				// RC5_64RB_Encrypt(
 				// 		RC5_ENCRYPTION_ROUNDS, 
 				// 		device->subkeys, 
 				// 		link, 
 				// 		link);
-				PHEMAP_DEV_linkGen(device, 0, &device->Q, &device->Q);
-				device->counter++;
+				PHEMAP_DEV_linkGen(device, 0, &entity->Q, &entity->Q);
+				entity->counter++;
 			}
 		}
 
@@ -109,58 +111,11 @@ uint32_t PHEMAP_Device_getNextLink(
 	// 		device->subkeys, 
 	// 		link, 
 	// 		link);	
-	PHEMAP_DEV_linkGen(device, 0, &device->Q, &device->Q);
-	device->counter++;
+	PHEMAP_DEV_linkGen(device, 0, &entity->Q, &entity->Q);
+	entity->counter++;
 
-	memcpy(link, &device->Q,sizeof(PHEMAP_Link_t));
+	memcpy(link, &entity->Q,sizeof(PHEMAP_Link_t));
 	printf("genero il link\n");	
 
 	return 0;
-}
-
-/**
- * @brief Update Q with the i-th non-sentinel link on the current chain.
- *
- * @details
- * Let us assume that D and AS are synchronized on the link 
- * @f$ l_{k} \in \gamma_{D,l0,M} @f$ after one of the PHEMAP functions - this 
- * means that @f$ l{k} @f$ is stored in Q - this function computes 
- * @f$ Q \gets \phi_{D}^{i}(Q) = l_{k+i} @f$.
- *
- * @note
- * A call to this function DOES alter the value of the Q secure register.
- *
- * @param [in]	device	
- * 				Pointer to the PHEMAP_Device_t instance which collect all
- * 				the device-related protocol information.
- *
- * @param [in]	i
- * 				Index of the requested link on the current chain.
- *
- * @note If the requested i-th link - @f$ l_{k+i} @f$ - is a sentinel link, the 
- * Q register will be updated with @f$ l_{k+i+1} @f$.
- * 					
- * @return The function returns the index of the closest non-sentinel link to
- * the requested one. It is equal to the @ref i parameter, but if the requested 
- * i-th link - @f$ l_{k+i} @f$ - is a sentinel link, the function will return 
- * i+1.
- */
-uint32_t PHEMAP_Device_updateQ(
-		PHEMAP_Device_t * const device,
-		uint32_t i)
-{
-
-	if (0 == ((device->counter + i) % SENTINEL))
-		i++;
-
-	device->counter+=i;
-
-	for (uint32_t k = 0; k < i; k++)
-		RC5_64RB_Encrypt(
-				RC5_ENCRYPTION_ROUNDS, 
-				device->subkeys, 
-				device->Q, 
-				device->Q);
-
-	return i;
 }
