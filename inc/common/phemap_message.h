82ce75d8e21b58d10c274ae1aec6e070da74b144
2330647 <alfonsofezza93@gmail.com>
2020-03-18 15:17:17 +0100

    added babel chain message type to enum

--- inc/common/phemap_message.h
+++ inc/common/phemap_message.h
@@ -50,25 +50,31 @@ typedef enum PHEMAP_PROTOCOL_MESSAGE_TYPE_T
 	verification_request = 5, /**< Request sent by the AS or the D in order to
 							       authenticate the counterpart */
 
-	verification_reply = 6, /**< Reply for a verification request */
+	verification_ack = 6, /**< Reply for a verification request */
 
-	salt_request = 7, /**< Salted PHEMAP carnet request, sent by D to AS */
+	bc_setup_request = 7, /**< Babel-chain PHEMAP setup request, sent 
+									    by D1 to the AS. */
 
-	salt_reply = 8, /**< Salted PHEMAP reply, sent by AS to D */
+	bc_setup_reply = 8, /**< Babel-chain PHEMAP carnet, sent by AS to D1 */
 
-	salt_ack = 9, /**< Salted PHEAMP ack, sent by D to AS, while entering in
+	bc_setup_ack = 9, /**< Babel-chain PHEMAP carnet, sent by D2 to AS */
+
+	bc_verification_request = 10, /**< Babel-chain verification request */
+
+	bc_verification_ack = 11, /**< Babel-chain verification ack */
+
+	salt_request = 12, /**< Salted PHEMAP carnet request, sent by D to AS */
+
+	salt_reply = 13, /**< Salted PHEMAP reply, sent by AS to D */
+
+	salt_ack = 14, /**< Salted PHEAMP ack, sent by D to AS, while entering in
 					   salted mode */
 
-	salted_carnet = 10, /**< Salted PHEMAP carnet. Sent by AS to G */
+	salted_carnet = 15, /**< Salted PHEMAP carnet. Sent by AS to G */
 
-	salt_invalidation = 11, /**< Salt invalidation message, sent bu D to AS */
+	salt_invalidation = 16, /**< Salt invalidation message, sent bu D to AS */
 
-	babelchain_setup_request = 12, /**< Babel-chain PHEMAP setup request, sent 
-									    by D to the AS. */
-
-	babelchain_carnet = 13, /**< Babel-chain PHEMAP carnet, sent by AS to D */
-
-	application = 14 /**< Application message, built on top of PHEMAP */
+	application = 17 /**< Application message, built on top of PHEMAP */
 }
 PHEMAP_Message_Type_t;
 
@@ -97,21 +103,6 @@ PHEMAP_Init_Ack_t;
 
 typedef struct
 {
-	PHEMAP_Device_ID_t device_ID;
-	PHEMAP_Link_t l_i;
-	uint32_t carnet_length;
-}
-PHEMAP_Carnet_Requ_t;
-
-typedef struct
-{
-	PHEMAP_Link_t l_1;
-	PHEMAP_Link_t l_2;
-}
-PHEMAP_Salt_Reply_t;
-
-typedef struct
-{
 	PHEMAP_Link_t l_1;
 	PHEMAP_Link_t body;
 }
@@ -120,9 +111,36 @@ PHEMAP_Verification_Requ_t;
 typedef struct
 {
 	PHEMAP_Link_t l_2;
-	PHEMAP_Link_t body;
 }
-PHEMAP_Verification_Reply_t;
+PHEMAP_Verification_Ack_t;
+
+typedef struct
+{
+	PHEMAP_Link_t l_i;
+	PHEMAP_Device_ID_t device_ID;
+	uint32_t carnet_length;
+}
+PHEMAP_Carnet_Requ_t;
+
+typedef struct
+{
+	PHEMAP_Link_t l_i;
+	PHEMAP_Carnet_t carnet;
+}
+PHEMAP_Carnet_Reply_t;
+
+typedef struct
+{
+	PHEMAP_Link_t l_carlen;
+}
+PHEMAP_Carnet_Ack_t;
+
+typedef struct
+{
+	PHEMAP_Link_t l_1;
+	PHEMAP_Link_t l_2;
+}
+PHEMAP_Salt_Reply_t;
 
 /**
  * @brief PHEMAP message.
@@ -164,7 +182,7 @@ typedef struct PHEMAP_PROTOCOL_MESSAGE
 			@f$\lbrace l_1; \; l_2 \rbrace = \lbrace l_2; m \oplus l_{i+S+2} \rbrace@f$,
 			if the message is an initialization ack; */ //TODO
 
-		PHEMAP_Verification_Reply_t verify_reply; /**< 
+		PHEMAP_Verification_Ack_t verify_ack; /**< 
 			@f$\lbrace l_2; \; v_3 \rbrace = \lbrace l_2; m \oplus l_{i+S+2} \rbrace@f$,
 			if the message is an initialization ack; */ //TODO
 
@@ -173,14 +191,18 @@ typedef struct PHEMAP_PROTOCOL_MESSAGE
 			request, or @f$\lbrace a_i; B, T \rbrace@f$, if the message is a 
 			Babel-chain setup request;
 			*/
-		
+
+		PHEMAP_Carnet_Reply_t carnet_reply; /**< A PHEMAP salted carnet
+			@f$\lbrace D; X_{D,x_0,T} \rbrace@f$, or a babel-chain carnet
+			@f$\lbrace \tau_{A,a_0,T,B,b_0,T} \rbrace@f$ */
+
+		PHEMAP_Carnet_Ack_t carnet_ack; /**< 
+			@f$\lbrace l_2; \; v_3 \rbrace = \lbrace l_2; m \oplus l_{i+S+2} \rbrace@f$,
+			if the message is an initialization ack; */ //TODO
+
 		PHEMAP_Salt_Reply_t salt_inst_reply; /**<
 			@f$\lbrace l_{i+1}; l_{i+2} \oplus S_D \rbrace@f$, if the message is
 			a salt-installation reply;*/
-
-		PHEMAP_Carnet_t carnet; /**< A PHEMAP salted carnet
-			@f$\lbrace D; X_{D,x_0,T} \rbrace@f$, or a babel-chain carnet
-			@f$\lbrace \tau_{A,a_0,T,B,b_0,T} \rbrace@f$ */
 
 	} payload;
 }
