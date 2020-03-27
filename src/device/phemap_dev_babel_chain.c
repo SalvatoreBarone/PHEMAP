7b1ae05a040b83de00a6d30c2a5b08270622930d
2330647 <alfonsofezza93@gmail.com>
2020-03-27 18:38:11 +0100

    added code for send and receive babel chain

--- src/device/phemap_dev_babel_chain.c
+++ src/device/phemap_dev_babel_chain.c
@@ -113,6 +113,7 @@ static int32_t PHEMAP_Device_checkBCSetupReply(
     memcpy( &device->send_ent.entity.Q,
             &link, 
             sizeof(PHEMAP_Link_t));
+	device->send_ent.entity.counter = device->verifier_ent.counter;
 
 	return 0;
 }
@@ -160,17 +161,231 @@ static void PHEMAP_Device_buildBCSetupAck(
 		PHEMAP_Device_t * const device,
 		PHEMAP_Message_t * const message)                     
 {
-    printf("init messaggio di BCSetup Ack\n");			//DELME
+    // printf("init messaggio di BCSetup Ack\n");			//DELME
     //inizializzo il messaggio
 	memset(message, 0, sizeof(PHEMAP_Message_t));
 
     message->type = bc_setup_ack;
 
-	printf("scrivo i campi del messaggio\n");			//DELME
+	// printf("scrivo i campi del messaggio\n");			//DELME
     PHEMAP_Device_getNextLink(device, 
 								&device->verifier_ent,
 								RET_NO_SENTINEL, 
 								&message->payload.carnet_ack.l_carlen);
+}
+
+/**
+ * @brief Builds a PHEMAP init reply message
+ *
+ * @param [in]	device	
+ * 				Pointer to the PHEMAP_Device_t instance which collect all
+ * 				the device-related protocol information.
+ *
+ * @param [in]	message
+ * 				Pointer to the received init-request message. It will contain
+ * 				the init-reply message.
+ *
+ * @param [in]	d_2
+ * 				This information will be used later, during the initialization
+ * 				procedure, to check the init ack message in order to 
+ * 				authenticate the AS.
+ */
+static int32_t PHEMAP_Device_buildBCVerificationRequest(
+		PHEMAP_Device_t * const device,
+		PHEMAP_Message_t * const message,
+		const PHEMAP_Link_t * const data)                     //questo tipo ovviamente va cambiatoTODO
+{
+	// printf("init messaggio\n");			//DELME
+    //inizializzo il messaggio
+	memset(message, 0, sizeof(PHEMAP_Message_t));
+
+	message->type = bc_verification_request;
+
+	if ((device->send_ent.carnet.count + 2) >= device->send_ent.carnet.length)
+	{						//controllo che ci siano ticket disponibili fino all'ack
+		return -1;
+	}
+
+	// printf("scrivo i campi del messaggio\n");			//DELME
+	PHEMAP_Link_t link;
+	memcpy( &link, 							//prendo il count-esimo elemento del carnet
+			&device->send_ent.carnet.tickets[device->send_ent.carnet.count++],
+			sizeof(PHEMAP_Link_t));
+// //-------DELME--------------------------------------------//
+// 	printf("CARNET link\n");			//DELME
+// 	uint8_t * print_ptr;
+	
+// 	print_ptr = (uint8_t*) &link;
+// 	for (int k = 0; k < sizeof(PHEMAP_Link_t); k++)
+// 	{
+// 		printf("%02x ", print_ptr[k]);
+// 	}
+// 	printf("\n");
+// 	//---------------------------------------------------//
+
+	PHEMAP_Device_getNextLink(device, 
+								&device->send_ent.entity, 
+								RET_NO_SENTINEL, 
+								&message->payload.verify_req.l_1);
+	// //---DELME------------------------------------------------//
+	
+	// print_ptr = (uint8_t*) &message->payload.verify_req.l_1;
+	// for (int k = 0; k < sizeof(PHEMAP_Link_t); k++)
+	// {
+	// 	printf("%02x ", print_ptr[k]);
+	// }
+	// printf("\n");
+	// // ---------------------------------------------------//
+	memxor(&message->payload.verify_req.l_1, &link, sizeof(PHEMAP_Link_t));
+
+	memcpy( &link, 							//prendo il count-esimo elemento del carnet
+			&device->send_ent.carnet.tickets[device->send_ent.carnet.count++],
+			sizeof(PHEMAP_Link_t));
+	memcpy(&message->payload.verify_req.body, data, sizeof(PHEMAP_Link_t));
+	memxor(&message->payload.verify_req.body, &link, sizeof(PHEMAP_Link_t));
+	PHEMAP_Device_getNextLink(device, 
+								&device->send_ent.entity, 
+								RET_NO_SENTINEL, 
+								&link);
+	memxor(&message->payload.verify_req.body, &link, sizeof(PHEMAP_Link_t));
+	
+	return 0;
+}
+
+/**
+ * @brief Checks if a received verification request is well formed. 
+ *
+ * @param [in]	chain
+ * 				Pointer to the currently in use PHEMAP chain instance.
+ *
+ * @param [in]	message
+ * 				Pointer to the received message.
+ *
+ * @param [in]	nonce
+ * 				Memory area in which the nonce, extracted from the init reply
+ * 				message, will be placed.
+ *
+ * @return Upon successful completion, 0 shall be returned; otherwise, -1 shall 
+ * be returned.
+ */
+static int32_t PHEMAP_Device_checkBCVerificationRequest(
+		PHEMAP_Device_t * const device,
+		PHEMAP_Message_t * const message,
+		PHEMAP_Link_t * const data)                 //questo tipo ovviamente va cambiatoTODO
+{
+	// validazione del messaggio
+	if (bc_verification_request != message->type)
+		return -1;
+    
+	PHEMAP_Link_t link;
+	PHEMAP_Device_getNextLink(device, 
+								&device->recv_ent, 
+								RET_NO_SENTINEL, 
+								&link);
+
+// //------DELME---------------------------------------------//
+// 	uint8_t * print_ptr;
+	
+// 	print_ptr = (uint8_t*) &link;
+// 	for (int k = 0; k < sizeof(PHEMAP_Link_t); k++)
+// 	{
+// 		printf("%02x ", print_ptr[k]);
+// 	}
+// 	printf("\n");
+// //---------------------------------------------------//
+
+    if (0 != memcmp(&message->payload.verify_req.l_1,
+                    &link, 
+                    sizeof(PHEMAP_Link_t)))
+    {
+		return -1;
+    }
+
+	PHEMAP_Device_getNextLink(device, 
+								&device->recv_ent, 
+								RET_NO_SENTINEL, 
+								&link);
+
+	//valutare se fare inizializzazione di data 
+	memcpy( data,
+            &message->payload.verify_req.body,
+            sizeof(PHEMAP_Link_t));  
+	memxor(data, &link, sizeof(PHEMAP_Link_t));
+	
+    return 0;
+}
+
+/**
+ * @brief Create a new verification reply message. 
+ *
+ * @param [in]	chain
+ * 				Pointer to the currently in use PHEMAP chain instance.
+ *
+ * @param [in]	message
+ * 				Pointer to the PHEMAP message instance.
+ * 
+ * @param [in]	data
+ * 				Pointer to the data buffer container.
+ */
+static void PHEMAP_Device_buildBCVerificationAck(
+		PHEMAP_Device_t * const device,
+		PHEMAP_Message_t * const message)
+{
+	memset(message, 0, sizeof(PHEMAP_Message_t));
+
+	message->type = bc_verification_ack;
+
+	PHEMAP_Link_t link;
+	PHEMAP_Device_getNextLink(device, 
+								&device->recv_ent, 
+								RET_NO_SENTINEL, 
+								&link);
+
+	// Copia dell'attuale link della chain
+	memcpy( &message->payload.verify_ack.l_2, 
+			&link, 
+			sizeof(PHEMAP_Link_t));
+}
+
+/**
+ * @brief Checks if the intt ack message is well formed.
+ *
+ * @param [in]	device	
+ * 				Pointer to the PHEMAP_Device_t instance which collect all
+ * 				the device-related protocol information.
+ *
+ * @param [in]	message
+ * 				Pointer to the received init-request message. It will contain
+ * 				the init-reply message.
+ *
+ * @return Upon successful completion, 0 shall be returned; otherwise, -1 shall 
+ * be returned.
+ */
+static int32_t PHEMAP_Device_checkBCVerificationAck(
+		PHEMAP_Device_t * const device,
+		PHEMAP_Message_t * const message)
+{
+	if (bc_verification_ack != message->type)
+		return -1;
+
+    PHEMAP_Link_t link[2];
+	memcpy( &link[0], 							//prendo il count-esimo elemento del carnet
+			&device->send_ent.carnet.tickets[device->send_ent.carnet.count++],
+			sizeof(PHEMAP_Link_t));
+	PHEMAP_Device_getNextLink(device, 
+								&device->send_ent.entity, 
+								RET_NO_SENTINEL, 
+								&link[1]);
+	memxor(&link[0], &link[1], sizeof(PHEMAP_Link_t));
+
+    if (0 != memcmp(&message->payload.verify_ack.l_2,
+                    &link[0], 
+                    sizeof(PHEMAP_Link_t)))
+    {
+        return -1;
+    }
+
+	return 0;
 }
 
 /**
@@ -218,22 +433,24 @@ int32_t PHEMAP_Device_BCconnectToDev(
 		return -1;
 	}
 
-    /* timer creation */
-    linux_timer_t tim;
-    linux_create_timer(&tim, 100);			//fai una define diviso 5
-	int32_t elaps = 0;
-	int32_t res = 1;
-	// Si aspetta la ricezione della risposta 
-	while (res!=0 && elaps!=5)				//così conti 5 volte e sei più responsive
-	{
-        linux_wait_period(&tim);
-		res = ch_msg_pop(&device->chnl, &msg);
-		elaps++;
-	}	
-	if(res != 0)
-	{
-		return -1;
-	}
+	while (ch_msg_pop(&device->chnl, &msg)!=0);	
+
+    // /* timer creation */
+    // linux_timer_t tim;
+    // linux_create_timer(&tim, 100);			//fai una define diviso 5
+	// int32_t elaps = 0;
+	// int32_t res = 1;
+	// // Si aspetta la ricezione della risposta 
+	// while (res!=0 && elaps!=5)				//così conti 5 volte e sei più responsive
+	// {
+    //     linux_wait_period(&tim);
+	// 	res = ch_msg_pop(&device->chnl, &msg);
+	// 	elaps++;
+	// }	
+	// if(res != 0)
+	// {
+	// 	return -1;
+	// }
 
     printf("controllo la risposta Babel Chain\n");			//DELME
 	// Se la risposta viene ricevuta, si passa alla sua analisi per
@@ -245,12 +462,12 @@ int32_t PHEMAP_Device_BCconnectToDev(
 
     // Se l'installazione del carnet è andata a buon fine si 
     // allinea il registro Q del verifier al nuovo link
-    PHEMAP_Device_peekLink(device, 
-							&device->verifier_ent,
-							max_messages, 
-							RET_NO_SENTINEL, 
-							&device->verifier_ent.Q);
-    device->verifier_ent.counter += max_messages;
+    int senti_num = PHEMAP_Device_peekLink( device, 
+											&device->verifier_ent,
+											max_messages, 
+											RET_NO_SENTINEL, 
+											&device->verifier_ent.Q);
+    device->verifier_ent.counter = device->verifier_ent.counter + max_messages + senti_num;
     return 0;
 }
 
@@ -283,23 +500,25 @@ int32_t PHEMAP_Device_BCNotifyRecv(
 	msg.size = sizeof(PHEMAP_Message_t);
 
     printf("ricevo la notify Babel Chain Setup\n");			//DELME
-    /* timer creation */
-    linux_timer_t tim;
-    linux_create_timer(&tim, 100);			//fai una define diviso 5
-	int32_t elaps = 0;
-	int32_t res = 1;
-	// Si aspetta la ricezione della risposta 
-    res = ch_msg_pop(&device->chnl, &msg);
-	while (res!=0 && elaps!=5)				//così conti 5 volte e sei più responsive
-	{
-        linux_wait_period(&tim);
-		res = ch_msg_pop(&device->chnl, &msg);
-		elaps++;
-	}	
-	if(res != 0)
-	{
-		return -1;
-	}
+	while (ch_msg_pop(&device->chnl, &msg)!=0);	
+
+    // /* timer creation */
+    // linux_timer_t tim;
+    // linux_create_timer(&tim, 100);			//fai una define diviso 5
+	// int32_t elaps = 0;
+	// int32_t res = 1;
+	// // Si aspetta la ricezione della risposta 
+    // res = ch_msg_pop(&device->chnl, &msg);
+	// while (res!=0 && elaps!=5)				//così conti 5 volte e sei più responsive
+	// {
+    //     linux_wait_period(&tim);
+	// 	res = ch_msg_pop(&device->chnl, &msg);
+	// 	elaps++;
+	// }	
+	// if(res != 0)
+	// {
+	// 	return -1;
+	// }
 
     uint32_t carnet_length = 0;
     PHEMAP_Message_t * message;
@@ -313,19 +532,19 @@ int32_t PHEMAP_Device_BCNotifyRecv(
 		return -1;
 	}
 
-    // copio il valore del registro Q del verifier in in quello per la ricezione
+    // copio il valore del registro Q del verifier in quello per la ricezione
     memcpy( &device->recv_ent.Q,
 			&device->verifier_ent.Q,
 			sizeof(PHEMAP_Link_t));
 	device->recv_ent.counter = device->verifier_ent.counter;
 
-    // allineo il registro Q del verifier al nuovo link
-    PHEMAP_Device_peekLink(device, 
-							&device->verifier_ent,
-							carnet_length, 
-							RET_NO_SENTINEL, 
-							&device->verifier_ent.Q);
-    device->verifier_ent.counter += carnet_length;
+	// allineo il registro Q del verifier al nuovo link
+    uint32_t senti_num = PHEMAP_Device_peekLink(device, 
+												&device->verifier_ent,
+												carnet_length, 
+												RET_NO_SENTINEL, 
+												&device->verifier_ent.Q);
+    device->verifier_ent.counter = device->verifier_ent.counter + carnet_length + senti_num;
 
     printf("costruisco l'ack Babel Chain \n");			//DELME
 	// Si costruisce la richiesta al verifier	
@@ -342,124 +561,114 @@ int32_t PHEMAP_Device_BCNotifyRecv(
     return 0;
 }
 
-// /**
-//  * @brief Close the connection with the peer.
-//  *
-//  * @param [in]	device	
-//  * 				Pointer to the PHEMAP_Device_t instance which collect all
-//  * 				the device-related protocol information.
-//  *
-//  * @return Upon successful completion, 0 shall be returned; otherwise, -1 shall 
-//  * be returned.
-//  */
-// inline int32_t PHEMAP_Device_closePeerConnection(PHEMAP_Device_t * const device)
-// {
-// 	memset(&device->carnet, 0, sizeof(PHEMAP_Carnet_t));
-// 	return client_close(&device->peer);
-// }
+int32_t PHEMAP_Device_BCsend(
+		PHEMAP_Device_t * const device,
+		PHEMAP_Device_ID_t dest_id,
+		PHEMAP_Link_t * const data)
+{
+    ch_msg_t msg;
+	msg.cli_id = dest_id;
+	msg.size = sizeof(PHEMAP_Message_t);
 
-// /**
-//  * @brief Sends data to a peer device.
-//  *
-//  * @param [in]	device	
-//  * 				Pointer to the PHEMAP_Device_t instance which collect all
-//  * 				the device-related protocol information.
-//  *
-//  * @param [in]	data
-//  * 				Pointer to the memory bufer holding data to be sent.
-//  *
-//  * @param [in]	size
-//  * 				Size of the data to be sent..
-//  *
-//  * @return Upon successful completion, the function shall return the number of 
-//  * bytes sent. Otherwise, -1 shall be returned.
-//  */
-// ssize_t PHEMAP_Device_sendToPeer(
-// 		PHEMAP_Device_t * const device,
-// 		const uint8_t * const data,
-// 		size_t size)
-// {
-// 	if (device->carnet.counter == device->carnet.length)
-// 		return -1;
+	PHEMAP_Message_t * message;
+    message = (PHEMAP_Message_t*)msg.buff;	//casto a questo tipo così da risparmiare una copia
 
-// 	// Ottengo b_j dal carnet di tickets
-// 	memxor(
-// 			&device->carnet.tickets[device->counter], 
-// 			device->Q, 
-// 			sizeof(PHEMAP_Link_t));
+    printf("costruisco la Babel Chain verify\n");			//DELME
+	// Si costruisce la richiesta di verifica	
+    PHEMAP_Device_buildBCVerificationRequest(device, message, data);
 
-	
-// 	uint8_t * data_ptr = (uint8_t*) data;
-// 	uint8_t * l = (uint8_t *) &device->carnet.tickets[device->counter];
+    printf("invio la richiesta Babel Chain verify\n");			//DELME
+	// Si invia la richiesta al verifier
+    // while (ch_msg_push(&device->chnl, &msg)!=0);
+    if(ch_msg_push(&device->chnl, &msg)!=0)
+	{
+		return -1;
+	}
 
+	// Si aspetta la ricezione della risposta 
+    printf("attendo l'ack Babel Chain verify\n");			//DELME
+	while (ch_msg_pop(&device->chnl, &msg)!=0);	
 
-// 	// Xor con il tichet b_j 
-// 	for (size_t i = 0, j = 0; i < size; i++, j = (j + 1) % sizeof(PHEMAP_Link_t)) 
-// 		data_ptr[i] ^= l[j];
+    // /* timer creation */
+    // linux_timer_t tim;
+    // linux_create_timer(&tim, 100);			//fai una define diviso 5
+	// int32_t elaps = 0;
+	// int32_t res = 1;
+	// // Si aspetta la ricezione della risposta 
+	// while (res!=0 && elaps!=5)				//così conti 5 volte e sei più responsive
+	// {
+    //     linux_wait_period(&tim);
+	// 	res = ch_msg_pop(&device->chnl, &msg);
+	// 	elaps++;
+	// }	
+	// if(res != 0)
+	// {
+	// 	return -1;
+	// }
 
-// 	ssize_t status = client_send(&device->peer, data, size);
+    printf("controllo l'ack Babel Chain verify\n");			//DELME
+	// Se la risposta viene ricevuta, si passa alla sua analisi per
+	// completare la procedura di installazione del carnet
+	if (0 !=  PHEMAP_Device_checkBCVerificationAck(device, message))
+	{
+		return -1;
+	}
 
-// 	// Ripristino dei dati
-// 	for (size_t i = 0, j = 0; i < size; i++, j = (j + 1) % sizeof(PHEMAP_Link_t)) 
-// 		data_ptr[i] ^= l[j];
+    return 0;
+}
 
-// 	if ((-1 != status) && (0 != status))
-// 	{
-// 		PHEMAP_Device_updateQ(device, 1);
-// 		device->carnet.counter++;
-// 	}
+int32_t PHEMAP_Device_BCrecv(
+		PHEMAP_Device_t * const device,
+		PHEMAP_Device_ID_t src_id,
+		PHEMAP_Link_t * const data)
+{
+    ch_msg_t msg;
+	msg.cli_id = src_id;
+	msg.size = sizeof(PHEMAP_Message_t);
 
-// 	return status;
-// }
+	PHEMAP_Message_t * message;
+    message = (PHEMAP_Message_t*)msg.buff;	//casto a questo tipo così da risparmiare una copia
 
-// /**
-//  * @brief Receive data from a peer device.
-//  *
-//  * @param [in]	device	
-//  * 				Pointer to the PHEMAP_Device_t instance which collect all
-//  * 				the device-related protocol information.
-//  *
-//  * @param [in]	data
-//  * 				Pointer to the memory bufer in which (eventually) received data 
-//  * 				have to be stored. This buffer must be pre-allocated and its
-//  * 				size must be greater (or equal) than the size of the data.
-//  *
-//  * @param [in]	size
-//  * 				Size of the data to be received.
-//  *
-//  * @return Upon successful completion, the function shall return the length of 
-//  * the received message in bytes. If no messages are available to be received 
-//  * and the peer has performed an orderly shutdown, the function shall return 0. 
-//  * Otherwise, -1 shall be returned.
-//  */
-// ssize_t PHEMAP_Device_recvFromPeer(
-// 		PHEMAP_Device_t * const device,
-// 		uint8_t * const data,
-// 		size_t size)
-// {
-// 	if (device->carnet.counter == device->carnet.length)
-// 		return -1;
+	// Si aspetta la ricezione della risposta 
+    printf("attendo la Babel Chain verify req\n");			//DELME
+	while (ch_msg_pop(&device->chnl, &msg)!=0);	
 
-// 	ssize_t status = client_recv(&device->peer, data, size);
+    // /* timer creation */
+    // linux_timer_t tim;
+    // linux_create_timer(&tim, 100);			//fai una define diviso 5
+	// int32_t elaps = 0;
+	// int32_t res = 1;
+	// // Si aspetta la ricezione della risposta 
+	// while (res!=0 && elaps!=5)				//così conti 5 volte e sei più responsive
+	// {
+    //     linux_wait_period(&tim);
+	// 	res = ch_msg_pop(&device->chnl, &msg);
+	// 	elaps++;
+	// }	
+	// if(res != 0)
+	// {
+	// 	return -1;
+	// }
 
-// 	if ((-1 != status) && (0 != status))
-// 	{
-// 		// Ottengo b_j dal carnet di tickets
-// 		memxor(
-// 				&device->carnet.tickets[device->counter], 
-// 				device->Q, 
-// 				sizeof(PHEMAP_Link_t));
+    printf("controllo la Babel Chain verify req\n");			//DELME
+	// Se la risposta viene ricevuta, si passa alla sua analisi per
+	// completare la procedura di installazione del carnet
+	if (0 !=  PHEMAP_Device_checkBCVerificationRequest(device, message, data))
+	{
+		return -1;
+	}
 
-// 		uint8_t * l = (uint8_t *) &device->carnet.tickets[device->counter];
+	printf("costruisco il Babel Chain verify ack\n");			//DELME
+	// Si costruisce l'ack	
+    PHEMAP_Device_buildBCVerificationAck(device, message);
 
-// 		// Cedrittazione 
-// 		for (size_t i = 0, j = 0; i < size; i++, j = (j + 1) % sizeof(PHEMAP_Link_t)) 
-// 			data[i] ^= l[j];
-		
-// 		PHEMAP_Device_updateQ(device, 1);
-// 		device->carnet.counter++;
-// 	}
+    printf("invio il Babel Chain verify ack\n");			//DELME
+	// Si invia l'ack al device
+    // while (ch_msg_push(&device->chnl, &msg)!=0);
+    if(ch_msg_push(&device->chnl, &msg)!=0)
+	{
+		return -1;
+	}
 
-// 	return status;
-// }
-
+    return 0;
+}
