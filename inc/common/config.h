2c374ae2d667372cdc754e10d7f17307af35e1f1
2330647 <alfonsofezza93@gmail.com>
2020-03-27 18:44:10 +0100

    some code fix

--- inc/common/config.h
+++ inc/common/config.h
@@ -4,7 +4,7 @@
 #include "phemap_message.h"
 
 #define CH_TH_WAIT_PERIOD   50
-#define DEVICE_NUM          2
+#define DEVICE_NUM          3
 #define MSG_SIZE            sizeof(PHEMAP_Message_t)
 #define SENTINEL            4
 #define DEV_FANIN           1
